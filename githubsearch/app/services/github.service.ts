17739a79c899e9130630a9e65a228dab286968d0
Gene Higgins <ehigginsiii@cnm.edu>
2016-10-11 14:08:00 -0600

    added github search

--- githubsearch/app/services/github.service.ts
+++ githubsearch/app/services/github.service.ts
@@ -7,14 +7,31 @@ export class GithubService {
   private username: string;
   private client_id = '07946ffd3dab210ac8a9';
   private client_secret = 'aab10a5a5b59b31160711a8131ecedf28d4fa720';
+  // private client_id: string;
+  // private client_secret: string;
 
   constructor(private _http: Http){
+    // this._http.get('./keys.json')
+    //   .map((res: Response) => {
+    //     res.json()
+    //       .subscribe( res => console.log("howdy"), error => console.log("error"))
+    //   });
+
     console.log('Github Service ready');
     this.username = 'eugenehiggins';
   }
 
   getUser(){
-    return this._http.get('http://api.github.com/users' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+    return this._http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
       .map(res => res.json());
+  }
+
+  getRepos(){
+    return this._http.get('http://api.github.com/users/' + this.username + '/repos?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+      .map(res => res.json());
+  }
+
+  updateUser(username:string){
+    this.username = username;
   }
 }
