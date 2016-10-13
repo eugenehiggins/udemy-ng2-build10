c33a97c273d39e38f30cea74f7ea9d3bcebb4706
Gene Higgins <ehigginsiii@cnm.edu>
2016-10-13 09:12:17 -0600

    expecting conflicts

--- mean-todo/routes/todos.js
+++ mean-todo/routes/todos.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://gh:gh@ds057386.mlab.com:57386/mean-todos',['todos']);
+var keys = require('../keys.json');
+
+console.log(`mongodb://${keys.user}:${keys.pass}@ds057386.mlab.com:57386/mean-todos`);
+var db = mongojs(`mongodb://${keys.user}:${keys.pass}@ds057386.mlab.com:57386/mean-todos`,['todos']);
 
 // Get Toddos
 router.get('/todos', function(req,res,next){
